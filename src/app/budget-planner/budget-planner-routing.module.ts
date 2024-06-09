fffe4277a3c31bfbbfdf5f1e653c0520c0e732ba
Franklin Palma <pfran7280@gmail.com>
2024-06-09 18:02:28 +0200

    added dashboard,income,expense,todo,history,profile

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeComponent } from './income/income.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { TodoComponent } from './todo/todo.component';
+import { HistoryComponent } from './history/history.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
@@ -9,7 +15,25 @@ const routes: Routes = [
   },
   {
     path:'side-nav', component: SideNavComponent
-  }
+  },
+  {
+    path:'dashboard', component: DashboardComponent
+  },
+  {
+    path:'income', component: IncomeComponent
+  },
+  {
+    path:'expense', component: ExpenseComponent
+  },
+  {
+    path:'todo', component: TodoComponent
+  },
+  {
+    path:'history', component: HistoryComponent
+  },
+  {
+    path:'profile', component: ProfileComponent
+  },
 ];
 
 @NgModule({
